854560315fc412a21aceca35f7007af5c63c64ae
rCotche <roshan.cotche@gmail.com>
2025-03-19 17:02:08 +0400

    1:07:06, camera mechanic ok, now row rotation mechanic

--- scenes/entities/player/camera_controller.gd
+++ scenes/entities/player/camera_controller.gd
@@ -1,5 +1,14 @@
 extends Node3D
 
+@export var horizontal_acceleration := 2.0
+@export var vertical_acceleration := 1.0
+
+func _process(delta: float) -> void:
+	#ps5 connected: not working
+	#ps5 connected & ds4windows lancé: not working
+	var joy_dir = Input.get_vector("pan_left", "pan_right", "pan_up", "pan_down")
+	rotate_from_vector(joy_dir * delta * Vector2(horizontal_acceleration, vertical_acceleration))
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		#print(event.relative)
@@ -8,6 +17,11 @@ func _input(event: InputEvent) -> void:
 
 func rotate_from_vector(v: Vector2):
 	if v.length() == 0:return
+	#horizontal rotation
 	rotation.y += v.x
+	#vertical rotation
+	rotation.x += v.y
+	#limit the rotation
+	rotation.x = clamp(rotation.x, -0.1,0.1)
 	#invert camera movement
 	#rotation.y -= v.x
