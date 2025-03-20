e2dbb28d2f6e44e9525665482191f049ef3368db
rCotche <roshan.cotche@gmail.com>
2025-03-20 09:26:11 +0400

    1:21:51, precise jump, je vais refractor le code

--- scenes/entities/player/camera_controller.gd
+++ scenes/entities/player/camera_controller.gd
@@ -1,7 +1,12 @@
 extends Node3D
 
+#rotation camera haut/bas
+@export var min_limit_x: float
+@export var max_limit_x: float
+
 @export var horizontal_acceleration := 2.0
 @export var vertical_acceleration := 1.0
+@export var mouse_acceleration := 0.005
 
 func _process(delta: float) -> void:
 	#ps5 connected: not working
@@ -13,15 +18,15 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		#print(event.relative)
 		#rotation.y += event.relative.x *0.005
-		rotate_from_vector(event.relative * 0.005)
+		rotate_from_vector(event.relative * mouse_acceleration)
 
 func rotate_from_vector(v: Vector2):
 	if v.length() == 0:return
 	#horizontal rotation
-	rotation.y += v.x
+	rotation.y -= v.x
 	#vertical rotation
-	rotation.x += v.y
+	rotation.x -= v.y
 	#limit the rotation
-	rotation.x = clamp(rotation.x, -0.1,0.1)
+	rotation.x = clamp(rotation.x, min_limit_x, max_limit_x)
 	#invert camera movement
 	#rotation.y -= v.x
--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,14 +1,35 @@
 extends CharacterBody3D
 
+#jump
+@export var jump_height : float = 2.25
+@export var jump_time_to_peak : float = 0.4
+@export var jump_time_to_descent : float = 0.3
+
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+# source: https://youtu.be/IOe1aGY6hXA?feature=shared
+
 #@export var base_speed: float = 4.0
 @export var base_speed := 4.0
 
 var movement_input := Vector2.ZERO
 
-func _physics_process(_delta: float) -> void:
-	movement_input = Input.get_vector("left", "right", "forward", "backward")
+@onready var camera = $CameraController/Camera3D
+
+#basic movement influence by a camera
+func _physics_process(delta: float) -> void:
+	movement_input = Input.get_vector("left", "right", "forward", "backward").rotated(-camera.global_rotation.y)
 	
-	#movement_input est en 2d donc on es obliger de créer un Vector3
-	velocity = Vector3(movement_input.x, 0, movement_input.y) * base_speed
+	#jump
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_velocity
+	#var gravity
+	#if velocity.y > 0.0:
+		#gravity = jump_gravity
+	#else:
+		#gravity = fall_gravity
+	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
+	velocity.y -= gravity * delta
 	
 	move_and_slide()
