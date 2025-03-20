787772c478d24c7022be28abede29a89561136a1
rCotche <roshan.cotche@gmail.com>
2025-03-20 11:16:00 +0400

    1:30:07, exercise

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -12,6 +12,7 @@ extends CharacterBody3D
 
 #@export var base_speed: float = 4.0
 @export var base_speed := 4.0
+@export var run_speed := 6.0
 
 var movement_input := Vector2.ZERO
 
@@ -19,17 +20,42 @@ var movement_input := Vector2.ZERO
 
 #basic movement influence by a camera
 func _physics_process(delta: float) -> void:
-	movement_input = Input.get_vector("left", "right", "forward", "backward").rotated(-camera.global_rotation.y)
-	
+	move_logic(delta)
 	#jump
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = -jump_velocity
-	#var gravity
-	#if velocity.y > 0.0:
-		#gravity = jump_gravity
-	#else:
-		#gravity = fall_gravity
-	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
-	velocity.y -= gravity * delta
+	jump_logic(delta)
 	
 	move_and_slide()
+
+func move_logic(delta: float) -> void:
+	movement_input = Input.get_vector("left", "right", "forward", "backward").rotated(-camera.global_rotation.y)
+	#1. Current movement speed du player
+	var velocity_2d := Vector2(velocity.x, velocity.z)
+	
+	#2. est ce que il y a un input
+	if movement_input != Vector2.ZERO:
+		#2.1 slowly accelerating from iur current speed vers la direction de l'input
+		velocity_2d += movement_input * base_speed * delta
+		#tous les vecteur ont la fonction limit_length
+		#2.2 set a maximum speed
+		velocity_2d = velocity_2d.limit_length(base_speed)
+		#Rappel velocity c'est un vecteur 3d
+		#2.3 updating velocity to get a new movement speed
+		velocity.x = velocity_2d.x
+		velocity.z = velocity_2d.y
+	#3 pas de input
+	else:
+		#permet de ralentir le perso lorsqu'il y a pas d'input
+		#3.1 get our current speed and change it to zero
+		#base_speed * 4.0 * delta c'est la vitesse à laquelle
+		#la vitesse du player va atteindre 0
+		velocity_2d = velocity_2d.move_toward(Vector2.ZERO, base_speed * 4.0 * delta)
+		#3.2 updating velocity to get a new movement speed
+		velocity.x = velocity_2d.x
+		velocity.z = velocity_2d.y
+
+func jump_logic(delta: float) -> void:
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -jump_velocity
+	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
+	velocity.y -= gravity * delta
