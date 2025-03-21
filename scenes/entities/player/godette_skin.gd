ff2e1c4d1ae79010657ed26f906ff7e60763f5f3
rCotche <roshan.cotche@gmail.com>
2025-03-21 19:13:29 +0400

    2:28:51

--- scenes/entities/player/godette_skin.gd
+++ scenes/entities/player/godette_skin.gd
@@ -10,3 +10,10 @@ func attack() -> void:
 	#play the animation once
 	#$AnimationTree.set("parameters/AttackOneShot/request", true) #mm chose
 	$AnimationTree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+func defend() -> void:
+	var tween = create_tween()
+	tween.tween_method(_defend_change, 1.0 - float(forward), float(forward), 0)
+
+func _defend_change(value: bool) -> void:
+	$AnimationTree.set("parameters/ShieldBlend/blend_amount", value)
