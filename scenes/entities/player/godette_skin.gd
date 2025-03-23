8ed363f08322a824399e193fdcc06691d4d7fc93
rCotche <roshan.cotche@gmail.com>
2025-03-23 10:31:40 +0400

    2:48:13, exercise

--- scenes/entities/player/godette_skin.gd
+++ scenes/entities/player/godette_skin.gd
@@ -1,15 +1,23 @@
 extends Node3D
 
 @onready var move_state_machine = $AnimationTree.get("parameters/MoveStateMachine/playback")
+@onready var attack_state_machine = $AnimationTree.get("parameters/AttackStateMachine/playback")
+
+var attacking := false
 
 func set_move_state(state_name: String) -> void:
 	move_state_machine.travel(state_name)
 
 func attack() -> void:
-	#set(): Assigns value to the given property
-	#play the animation once
-	#$AnimationTree.set("parameters/AttackOneShot/request", true) #mm chose
-	$AnimationTree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	if not attacking:
+		attack_state_machine.travel('Slice' if $SecondAttackTimer.time_left else 'Chop')
+		#set(): Assigns value to the given property
+		#play the animation once
+		#$AnimationTree.set("parameters/AttackOneShot/request", true) #mm chose
+		$AnimationTree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+func attack_toggle(value: bool) -> void:
+	attacking = value
 
 func defend(forward: bool) -> void:
 	var tween = create_tween()
