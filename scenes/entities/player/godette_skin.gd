5bfe2939f23909f362fa62aa17ac0458ab186cc3
rCotche <roshan.cotche@gmail.com>
2025-03-23 20:55:42 +0400

    3:26:32, animation part done

--- scenes/entities/player/godette_skin.gd
+++ scenes/entities/player/godette_skin.gd
@@ -3,6 +3,7 @@ extends Node3D
 @onready var move_state_machine = $AnimationTree.get("parameters/MoveStateMachine/playback")
 @onready var attack_state_machine = $AnimationTree.get("parameters/AttackStateMachine/playback")
 @onready var extra_animation = $AnimationTree.get_tree_root().get_node("ExtraAnimation")
+@onready var face_material: StandardMaterial3D = $Rig/Skeleton3D/Godette_Head.get_surface_override_material(0)
 
 #func _ready() -> void:
 	#print(extra_animation)
@@ -14,6 +15,13 @@ var squash_and_stretch := 1.0:
 		#3:16:24
 		var negative  = 1.0 + (1.0 - squash_and_stretch)
 		scale = Vector3(negative,squash_and_stretch,negative)
+const faces = {
+	'default': Vector3.ZERO,
+	'blink': Vector3(0,0.5,0),
+	'happy': Vector3(0.5,0.5,0),
+	'angry': Vector3(0.5,0,0),
+}
+var rng = RandomNumberGenerator.new()
 
 func set_move_state(state_name: String) -> void:
 	move_state_machine.travel(state_name)
@@ -57,3 +65,13 @@ func hit() -> void:
 	#cancel attack animation
 	$AnimationTree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT)
 	attacking = false
+
+func change_face(expression: String) ->void:
+	face_material.uv1_offset = faces[expression]
+
+
+func _on_blink_timer_timeout() -> void:
+	change_face('blink')
+	await get_tree().create_timer(0.2).timeout
+	change_face('default')
+	$BlinkTimer.wait_time = rng.randf_range(1.5, 3.0)
