ec8664f90622de55cc7a67120082c43c53e0f96b
rCotche <roshan.cotche@gmail.com>
2025-03-26 15:21:47 +0400

    3:52:11

--- scenes/entities/enemies/boss.gd
+++ scenes/entities/enemies/boss.gd
@@ -2,3 +2,11 @@ extends Enemy
 
 func _physics_process(delta: float) -> void:
 	move_to_player(delta)
+
+
+func _on_attack_timer_timeout() -> void:
+	if position.distance_to(player.position) < 5.0:
+		melee_attack_animation()
+
+func melee_attack_animation() -> void:
+	pass
