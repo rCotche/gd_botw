e41921d27d139182a7a3373f7592196c63ed6904
rCotche <roshan.cotche@gmail.com>
2025-03-28 18:56:28 +0400

    5:00:00

--- scenes/entities/enemies/skeleton_warrior.gd
+++ scenes/entities/enemies/skeleton_warrior.gd
@@ -13,3 +13,6 @@ func _on_attack_timer_timeout() -> void:
 	$Timers/AttackTimer.wait_time = rng.randf_range(2.0,3.0)
 	if position.distance_to(player.position) < attack_radius:
 		melee_attack_animation()
+
+func can_damage(value: bool) -> void:
+	$skin/Rig/Skeleton3D/BoneAttachment3D/Bone.can_damage = value
